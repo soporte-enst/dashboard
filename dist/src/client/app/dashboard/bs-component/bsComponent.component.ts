3125f6b87cc509088d4dbecb3ab7c50ecffb72e5
soporte-enst <soporte@enst.edu.mx>
2024-05-17 16:42:25 -0600

    Change project

--- dist/src/client/app/dashboard/bs-component/bsComponent.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	moduleId: module.id,
-    selector: 'bs-component',
-    templateUrl: './bs-component.component.html'
-})
-
-export class BSComponentComponent {}
--- src/client/app/shared/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This barrel file provides the exports for the shared resources (services, components).
- */
-export * from './sidebar/index';
-
-export * from './name-list/index';
-
-export * from './config/env.config';
