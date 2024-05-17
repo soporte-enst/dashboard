38f846fb0206626a05315f9218a3fe73b2b2a155
soporte-enst <soporte@enst.edu.mx>
2024-05-17 16:01:19 -0600

    Archivo cpanel 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: 'test1',
+    component: Test1Component
+    },
+    { path: 'test2',
+    component: Test2Component
+    },
+];
